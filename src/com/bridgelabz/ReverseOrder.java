c8a088f19d13bf1e06cb13d3ea3c94a6a737f32b
Prohit <udkarprohit@gmail.com>
2023-01-09 06:40:37 +0530

    Program6: displaying the elements of an array in reverse order

---
+++ src/com/bridgelabz/ReverseOrder.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class ReverseOrder {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+        System.out.print("Original array: ");
+        for (int i = 0;i < arr.length;i++)
+        {
+            System.out.print(arr[i]);
+        }
+        System.out.println("");
+        System.out.print("Array in reverse order : ");
+        for (int i = arr.length-1;i >= 0;i--)
+        {
+            System.out.print(arr[i]);
+        }
+
+    }
+}
